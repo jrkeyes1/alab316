a9819bb86b77ab2bddd6454b4f5fc501415c41fc
jrkeyes1 <jrkeyes1@gmail.com>
2024-07-10 22:13:56 -0400

    updated coe

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+
+//step 1
+const mainEl = document.querySelector('main');
+console.log(mainEl);
+//step 2
+mainEl.style.backgroundColor = 'var(--main-bg)'
+//let backGroundColor = 'var(--main-bg)'
+//step 3 
+mainEl.innerHTML = '<h1>DOM Manipulation</h1>'
+//step 4 
+mainEl.classList = 'flex-ctr';
+
